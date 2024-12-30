515e49d15dae69683c884cbfbbe001c7e6b55f7f
jun-suzuki1028 <calc.9029@gmail.com>
2024-12-31 07:01:28 +0900

    fix

--- lib/cdk-nag-test-stack.ts
+++ lib/cdk-nag-test-stack.ts
@@ -2,17 +2,22 @@ import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { Bucket } from "aws-cdk-lib/aws-s3";
 import { NagSuppressions } from "cdk-nag";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
 export class CdkNagTestStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-    NagSuppressions.addStackSuppressions(this, [
-      {
-        id: "AwsSolutions-S1",
-        reason: "This is a demo bucket, so it does not need access logs",
+
+    // VPCの作成
+    const vpc = new ec2.Vpc(this, "TestVpc", {
+      maxAzs: 2, // デフォルトの設定を使用
+    });
+
+    // ネットワークACLの作成
+    const networkAcl = new ec2.NetworkAcl(this, "TestNetworkAcl", {
+      vpc,
+      subnetSelection: {
+        subnetType: ec2.SubnetType.PUBLIC,
       },
-    ]);
-    const bucket = new Bucket(this, "Bucket", {
-      enforceSSL: true,
     });
   }
 }
