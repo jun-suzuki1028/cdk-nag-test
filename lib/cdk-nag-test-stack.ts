8e60c585556b2422d71537700db5187342176d54
jun-suzuki1028 <calc.9029@gmail.com>
2024-12-31 08:06:25 +0900

    fix

--- lib/cdk-nag-test-stack.ts
+++ lib/cdk-nag-test-stack.ts
@@ -1,19 +1,20 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { Bucket } from "aws-cdk-lib/aws-s3";
-import { NagSuppressions } from "cdk-nag";
 import * as ec2 from "aws-cdk-lib/aws-ec2";
 export class CdkNagTestStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // VPCの作成
-    const vpc = new ec2.Vpc(this, "TestVpc", {
-      maxAzs: 2, // デフォルトの設定を使用
+    const bucket = new Bucket(this, "Bucket", {
+      enforceSSL: true,
     });
 
-    // ネットワークACLの作成
-    const networkAcl = new ec2.NetworkAcl(this, "TestNetworkAcl", {
+    const vpc = new ec2.Vpc(this, "Vpc", {
+      maxAzs: 2,
+    });
+
+    const networkAcl = new ec2.NetworkAcl(this, "NetworkAcl", {
       vpc,
       subnetSelection: {
         subnetType: ec2.SubnetType.PUBLIC,
