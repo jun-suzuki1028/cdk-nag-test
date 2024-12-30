7393d26a104a85f8a62a201e356af7833a931861
jun-suzuki1028 <calc.9029@gmail.com>
2024-12-30 19:31:22 +0900

    fix

--- lib/cdk-nag-test-stack.ts
+++ lib/cdk-nag-test-stack.ts
@@ -5,12 +5,12 @@ import { NagSuppressions } from "cdk-nag";
 export class CdkNagTestStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-    NagSuppressions.addStackSuppressions(this, [
-      {
-        id: "AwsSolutions-S1",
-        reason: "This is a demo bucket, so it does not need access logs",
-      },
-    ]);
+    // NagSuppressions.addStackSuppressions(this, [
+    //   {
+    //     id: "AwsSolutions-S1",
+    //     reason: "This is a demo bucket, so it does not need access logs",
+    //   },
+    // ]);
     const bucket = new Bucket(this, "Bucket", {
       enforceSSL: true,
     });
