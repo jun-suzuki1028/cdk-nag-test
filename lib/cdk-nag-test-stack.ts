444ebeddc89aa41f07c62c190462b0cde0724cbc
jun-suzuki1028 <calc.9029@gmail.com>
2024-12-31 08:24:17 +0900

    fix

--- lib/cdk-nag-test-stack.ts
+++ lib/cdk-nag-test-stack.ts
@@ -2,23 +2,34 @@ import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { Bucket } from "aws-cdk-lib/aws-s3";
 import * as ec2 from "aws-cdk-lib/aws-ec2";
+import { NagSuppressions } from "cdk-nag";
 export class CdkNagTestStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const bucket = new Bucket(this, "Bucket", {
-      enforceSSL: true,
+    // const bucket = new Bucket(this, "Bucket", {
+    //   enforceSSL: true,
+    // });
+
+    const vpc = new ec2.Vpc(this, "Vpc", {
+      maxAzs: 2,
     });
 
-    // const vpc = new ec2.Vpc(this, "Vpc", {
-    //   maxAzs: 2,
-    // });
-
-    // const networkAcl = new ec2.NetworkAcl(this, "NetworkAcl", {
-    //   vpc,
-    //   subnetSelection: {
-    //     subnetType: ec2.SubnetType.PUBLIC,
-    //   },
-    // });
+    const networkAcl = new ec2.NetworkAcl(this, "NetworkAcl", {
+      vpc,
+      subnetSelection: {
+        subnetType: ec2.SubnetType.PUBLIC,
+      },
+    });
+    NagSuppressions.addResourceSuppressions(
+      vpc,
+      [
+        {
+          id: "AwsSolutions-VPC7",
+          reason: "This is a test",
+        },
+      ],
+      true,
+    );
   }
 }
